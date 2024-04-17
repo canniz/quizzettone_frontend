caa76b80666ae5d8f66a1a18841d539c52775d31
Tareq <tareqbayoumi92@gmail.com>
2024-04-17 03:17:40 +0200

    removed timeLeft from useEffect dependency since it cause useEffect to run each time timeLeft changes

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,17 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 function Timer({ duration, onTimeOut }) {
   const [timeLeft, setTimeLeft] = useState(duration);
 
   useEffect(() => {
-    if (!timeLeft) return onTimeOut();
-
+    // start timer
     const intervalId = setInterval(() => {
-      setTimeLeft(timeLeft - 1);
+      setTimeLeft((prev) => {
+        // stop the timer and call onTimeOut when timeLeft is 0
+        if (!prev) {
+          onTimeOut();
+          // clear interval when timeLeft is 0
+          clearInterval(intervalId);
+          return prev;
+        }
+        // decrease timeLeft by one second
+        return prev - 1;
+      });
     }, 1000);
-
+    // clear interval on unmount
     return () => clearInterval(intervalId);
-  }, [timeLeft, onTimeOut]);
+  }, []);
 
   return (
     <div>
