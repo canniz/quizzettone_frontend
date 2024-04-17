ea66b09faf392251f094c0bdcf082453e1f17d4f
cannizzu <giucannizzaro@gmail.com>
2024-04-17 20:42:11 +0200

    Aggiornato frontend rimosso reload della leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,37 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './Leaderboard.css'; // Ensure this is still imported
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./Leaderboard.css";
+import { RefreshIcon } from "../icons/RefreshIcon";
 
 function Leaderboard() {
-    const [scores, setScores] = useState([]);
-    const API_HOST = process.env.REACT_APP_API_HOST;
-    useEffect(() => {
-        // Get the current date
-        const now = new Date();
-        // Format the date as "game_YYYY_MM_DD"
-        const gameParam = `game_${now.getFullYear()}_${String(now.getMonth() + 1).padStart(2, '0')}_${String(now.getDate()).padStart(2, '0')}`;
-        axios.get(`${API_HOST}/scores?game=${gameParam}`)
-            .then(response => {
-                // Convert object to array and sort
-                const sortedScores = Object.entries(response.data)
-                    .sort((a, b) => b[1].score - a[1].score);
-                setScores(sortedScores);
-            })
-            .catch(error => console.error('Error fetching scores', error));
-    }, []);
+  const [scores, setScores] = useState([]);
+  const API_HOST = process.env.REACT_APP_API_HOST;
 
-    return (
-        <main className="page">
-            <h2>Leaderboard</h2>
-            <ul className="leaderboard-list">
-                {scores.map(([username, userScores], index) => (
-                    <li key={index} className="leaderboard-item">
-                        {username}: {userScores.score} points
-                    </li>
-                ))}
-            </ul>
-        </main>
-    );
+  // Function to fetch scores
+  const fetchScores = () => {
+    const now = new Date();
+    const gameParam = `game_${now.getFullYear()}_${String(
+      now.getMonth() + 1
+    ).padStart(2, "0")}_${String(now.getDate()).padStart(2, "0")}`;
+    axios
+      .get(`${API_HOST}/scores?game=${gameParam}`)
+      .then((response) => {
+        const sortedScores = Object.entries(response.data).sort(
+          (a, b) => b[1].score - a[1].score
+        );
+        setScores(sortedScores);
+      })
+      .catch((error) => console.error("Error fetching scores", error));
+  };
+
+  // UseEffect to run on mount
+  useEffect(() => {
+    fetchScores();
+  }, []);
+
+  return (
+    <main className="page">
+      <h2>Leaderboard</h2>
+      <button onClick={fetchScores} className="refresh_btn">
+        <RefreshIcon />
+      </button>
+      <ul className="leaderboard-list">
+        {scores.map(([username, userScores], index) => (
+          <li key={index} className="leaderboard-item">
+            {username}: {userScores.score} points
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
 }
 
 export default Leaderboard;
--- src/components/QuizNavbar.js
+++ src/components/QuizNavbar.js
@@ -1,31 +1,11 @@
 import "./nav.css";
-import { NavLink, useLocation } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function QuizNavbar() {
-  const location = useLocation();
-
-  const handleReload = (event, path) => {
-    if (location.pathname === path) {
-      event.preventDefault();
-      window.location.reload();
-    }
-  };
-
   return (
     <nav className="navbar">
-      <NavLink
-        to="/quiz"
-        onClick={(e) => handleReload(e, "/quiz")}
-        className="me-3" // Bootstrap spacing class
-      >
-        Quizzettone
-      </NavLink>
-      <NavLink
-        to="/leaderboard"
-        onClick={(e) => handleReload(e, "/leaderboard")}
-      >
-        Leaderboard
-      </NavLink>
+      <NavLink to="/quiz">Quizzettone</NavLink>
+      <NavLink to="/leaderboard">Leaderboard</NavLink>
     </nav>
   );
 }
